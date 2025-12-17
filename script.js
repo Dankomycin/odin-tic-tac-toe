08ec49ec6ac7b2bec284259ebedec7f848838af2
Dankomycin <212175585+Dankomycin@users.noreply.github.com>
2025-12-17 15:57:07 -0500

    Complete first initial gae logic

--- script.js
+++ script.js
@@ -1,58 +1,88 @@
 //const TicTacToe = (function (){
-    const gameboard = {
-        board: [],
-        create() {
+    const gameboard = (function () {
+        const board = [];
+
+        const createBoard = () => {
             for (let i = 0; i < 3; i++) {
-                this.board[i] = [];
+                board[i] = [];
                 for (let j = 0; j < 3; j++)
-                    this.board[i].push("");   
-        }},
-        updateBoard() {
-            return gameboard.board; 
-        },
-        placeMarker(row, column, player) {
-            if (this.board[row][column] === "") {
-                this.board[row][column] = player.marker;
-                this.updateBoard();
-            }
-        }}
+                    board[i].push("");   
+        }};
 
-    function createPlayer (name, marker) {
-        return {name, marker}
-    }
+        const getBoard = () => [...board];
 
-    const players = [{
-        name: "player1",
-        marker: "X"},
-        {name: "player2",
-        marker: "O"}]
-
-    const game = {
-        activePlayer: players[0],
-        hasEnded: false,
-        checkWin(){
-            const winningCombos = [ 0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001, 0b100010001, 0b001010100]
-            const gameboardInBinary = parseInt(gameboard.board
-                .flat()
-                .map((index) => index === this.activePlayer.marker ? '1': '0')
-                .join(''), 2)
-            if (winningCombos.find((int) => (gameboardInBinary & int) === int)) {
-                this.hasEnded = true;
-                console.log(`This ${this.activePlayer} has won the game!`);
-                return true;
-            }
-            return false
-        },
-        playerTurn(row, column) {
-            gameboard.placeMarker(row,column,this.activePlayer);
-            if (this.activePlayer === players[0]){
-                return this.activePlayer = players[1];
-            }
-            else {
-                return this.activePlayer = players[0];
+        const placeMarker = (row, column, marker) => {
+            if (board[row][column] === "") {
+                board[row][column] = marker;
             }
         }
-    }
+
+        return {createBoard, getBoard, placeMarker};
+    })();
+
+    const players = (function() {
+        const list = [];
+
+        const getPlayerList = () => [...list];
+
+        function Player(name, marker) {
+            this.name = name;
+            this.marker = marker;
+        };
+        function createPlayer(name, marker) {
+            const newPlayer = new Player(name, marker);
+            list.push(newPlayer);
+        };
+        return {getPlayerList, createPlayer}
+    })();
+
+    const game =(function() {
+        let activePlayer = {};
+        const getActivePlayer = () => activePlayer;
+        let hasEnded = false;
+        const checkWin = () => {
+            const winningCombos = [ 0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001, 0b100010001, 0b001010100]
+            const gameboardInBinary = parseInt(gameboard.getBoard()
+                .flat()
+                .map((index) => index === activePlayer.marker ? '1': '0')
+                .join(''), 2)
+            if (winningCombos.find((int) => (gameboardInBinary & int) === int)) {
+                hasEnded = true;
+                console.log(`${activePlayer} has won the game!`);
+                return true;
+            }if (gameboard.getBoard().flat().every((i) => i !== "")){
+                hasEnded = true;
+                console.log("The game is tied!")
+                return true;
+            }
+            return false;
+        };
+        const switchPlayer = () => {
+            activePlayer = activePlayer === players.getPlayerList()[0] ? players.getPlayerList()[1] : players.getPlayerList()[0];
+            console.log(`It is ${activePlayer.name}'s turn.`);
+        };
+        const startGame = () => {
+            players.createPlayer("Player1", "X");
+            players.createPlayer("Player2", "O");
+            activePlayer = players.getPlayerList()[0];
+            gameboard.createBoard();
+            console.log(`The gameboard is set!`);
+        }
+        const playRound = (row, column) => {
+            gameboard.placeMarker(row, column, activePlayer.marker);
+            gameboard.getBoard();
+            if(checkWin()) {return};
+            switchPlayer();
+        };
+        return {playRound, getActivePlayer, startGame};
+        })();
+
+    
+
+
+
+
+    
     
 
     
